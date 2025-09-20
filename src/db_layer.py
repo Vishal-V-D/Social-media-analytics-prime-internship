c3404c3303eb9cddf9ea05113a732d94b302f706
Vishal-V-D <vishalvd006@gmail.com>
2025-09-20 14:06:06 +0530

    feat(query): implement trending hashtag query

---
+++ src/db_layer.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from db.schema import User, Post, Hashtag, PostHashtag, Comment
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+engine = create_engine(f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
+Session = sessionmaker(bind=engine)
+
+def get_trending_hashtags():
+    session = Session()
+    results = session.query(
+        Hashtag.tag,
+        func.count(PostHashtag.post_id).label("freq")
+    ).join(PostHashtag).group_by(Hashtag.hashtag_id).order_by(func.count(PostHashtag.post_id).desc()).limit(5).all()
+    session.close()
+    return results
+
+if __name__ == "__main__":
+    for tag, freq in get_trending_hashtags():
+        print(tag, freq)
