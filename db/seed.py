de1130f6603f667169e931fb32bd263fd18f935b
Vishal-V-D <vishalvd006@gmail.com>
2025-09-21 20:16:04 +0530

    feat(db): add database seeding script with sample data

--- db/seed.py
+++ db/seed.py
@@ -3,12 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from schema import Base, User, Post, Hashtag, PostHashtag, Comment
 from dotenv import load_dotenv
 import os
+from urllib.parse import quote_plus
 
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+
 DB_NAME = os.getenv("DB_NAME")
 
 engine = create_engine(f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
